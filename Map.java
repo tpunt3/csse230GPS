a10b319d4314f73d6c747d0ce0aa590d6a8a7f75
stullam <stullam@rose-hulman.edu>
2015-02-07 15:48:18 -0500

    GMS
    Here are the Graph Map and Stars classes without the code in them just
    an outline

---
+++ Map.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Map {
+	
+	public ArrayList<Stars> starList;
+	
+	public Map() {
+		starList = new ArrayList<Stars>();
+	}
+	
+	//Displays states and Capitals
+	//Displays routes to be traveled
+
+}
---
+++ Stars.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+
+public class Stars {
+	
+	public Color currentColor;
+	
+	public Stars() {
+		currentColor = Color.BLACK;
+	}
+	
+	// Light up different colors depending on the best route
+	
+
+}

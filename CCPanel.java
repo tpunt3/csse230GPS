835f9268b4b1074ccd00359b3e5dd40aacaee5e9
kfclee <katherinefc.lee@gmail.com>
2015-02-07 15:19:18 -0500

    CCPanel Update

--- CCPanel.java
+++ CCPanel.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -7,19 +8,32 @@ import javax.swing.JPanel;
 
 
 public class CCPanel extends JPanel{
+	
+	ButtonPanel bPanel;
+	MapPanel mPanel;
 
 	public CCPanel(CCFrame frame){
+		
 		super();
-		this.setBackground(Color.GREEN);
-		JLabel title = new JLabel("Cross-Country Touring GPS");
-		title.setFont(new Font("Arial", Font.BOLD, 18));
-		this.add(title);
+		
+		BorderLayout layout = new BorderLayout();
+		this.setLayout(layout);
+
+		
+		this.bPanel = new ButtonPanel();		
+		layout.addLayoutComponent(bPanel, layout.NORTH);
+		this.add(bPanel);
+
+		this.mPanel = new MapPanel();
+		
+		
+		
+		this.setVisible(true);
 	}
 	
 	public void paintComponent(Graphics g){
 		//g.drawImage(img, x, y, observer);
 		//g.setColor(Color.WHITE);
 	}
-	
 	
 }

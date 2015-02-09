13063785e7db9acf1ce3e1d18c3c4f91721fe920
stullam <stullam@rose-hulman.edu>
2015-02-09 11:32:18 -0500

    Graph Class
    There are errors in this I just need other classes to be able to fix
    stuff

--- Graph.java
+++ Graph.java
@@ -6,10 +6,17 @@ public class Graph {
 	public ArrayList waypoints;
 	public CityClass target;
 	public CityClass currentCity;
+	public CityClass nextCity;
 	public int CrowFlyDist;
+	int xDist;
+	int yDist;
+	double distance;
 	
 	public Graph(){
 		waypoints = new ArrayList();
+		int xDist = 0;
+		int yDist = 0;
+		double distance = 0;
 	}
 	
 	public void SetCurrentCity(CityClass city) {
@@ -20,30 +27,48 @@ public class Graph {
 		this.target = finishPoint;
 	}
 	
-	public ArrayList ShortestDistanceRoute() {
+	public double ShortestConnection() {
 		// This will be an arrayList of all the cities to travel through
 		// to travel the shortest distance between two cities
+		int xDist = 0;
+		int yDist = 0;
+		double distance = 0;
+		
+		xDist = (Math.abs(currentCity.getX()) - Math.abs(nextCity.getX()));
+		yDist = (Math.abs(currentCity.getY()) - Math.abs(nextCity.getY()));
+		distance = Math.sqrt(xDist^2 + yDist^2);
 		
 		// xDist = math.abs(Point 2x - Point 1x) ^2
 		// yDist = math.abs(Point2y - Point 1y) ^2
 		// distance = math.sqrt(xDist^2 + yDist^2)
 		
-		return null;
+		
+		return distance;
 	}
 	
-	public void CalcDistanceRemaining() {
+	public double CalcDistanceRemaining() {
 		// calculates the as the crow flies distance remaining
 		// xDist = math.abs(currentCity X - target X);
 		// yDist = math.abs(currentCity Y - target Y);
 		
 		//CrowFlyDist = Math.sqrt(xDist^2 + yDist^2);
+		
+		int xDist = 0;
+		int yDist = 0;
+		double CrowFlyDistance = 0;
+		
+		xDist = (Math.abs(currentCity.getX()) - Math.abs(nextCity.getX()));
+		yDist = (Math.abs(currentCity.getY()) - Math.abs(nextCity.getY()));
+		CrowFlyDistance = Math.sqrt(xDist^2 + yDist^2);
+		
+		return CrowFlyDistance;
 	}
 	
-	public int GetDistanceRemaining() {
-		// returns CrowFliesDistance
-		
-		return CrowFlyDist;
-	}
+//	public int GetDistanceRemaining() {
+//		// returns CrowFliesDistance
+//		
+//		return CrowFlyDist;
+//	}
 	
 	public ArrayList ShortestTimeRoute() {
 		// This will be an arrayList of all the cities to travel through

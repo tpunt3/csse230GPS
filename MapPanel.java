d55387b502e86efeb435a274915c6b73b2f8dab8
kfclee <katherinefc.lee@gmail.com>
2015-02-12 20:55:50 -0500

    MapPanel

--- MapPanel.java
+++ MapPanel.java
@@ -1,5 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Panel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,7 +10,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class MapPanel extends Panel {
+public class MapPanel extends Panel implements MouseListener{
 	
 	private BufferedImage US;
 
@@ -18,6 +20,7 @@ public class MapPanel extends Panel {
 		ImageIcon USA = new ImageIcon("src\\map.png");
 		JLabel label = new JLabel("",USA,JLabel.CENTER);
 		this.add(label);
+		addMouseListener(this);
 		
 		this.setVisible(true);
 	}
@@ -26,6 +29,37 @@ public class MapPanel extends Panel {
 		super.paintComponents(go);
 		
 		go.drawImage(this.US, 0, 0, null);
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		System.out.println("X Coordinate: "+arg0.getX());
+		System.out.println("Y Coordinate: "+arg0.getY());
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 	

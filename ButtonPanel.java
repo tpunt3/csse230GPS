1980881531cfdc9a12f9a78ac41822a418e3d8e7
kfclee <katherinefc.lee@gmail.com>
2015-02-16 14:06:43 -0500

    ButtonPanel, EndBox, StartBox, CapitalInfo
    Now the drop boxes are fully functional and the text in the panels on
    the right side should change and be correct now!

--- ButtonPanel.java
+++ ButtonPanel.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Panel;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -16,22 +18,40 @@ public class ButtonPanel extends Panel {
 	private DirectionsPanel dPanel;
 	private POIPanel pPanel;
 
-	public ButtonPanel(MapPanel mP, DirectionsPanel dP, POIPanel pP){
+	public ButtonPanel(MapPanel mP, DirectionsPanel dP, POIPanel pP) throws IOException{
 		this.setBackground(Color.WHITE);
 		this.mPanel = mP;
 		this.dPanel = dP;
 		this.pPanel = pP;
+		
+		ReadFile file1 = new ReadFile(
+				"C:/EclipseWorkspaces/csse230/CrossCountryTouring/src/CapitalInfo");
+		String[] arrayOfLines = file1.OpenFile();
+		ArrayList<City> cities = new ArrayList<City>();
+		for (int i = 0; i < arrayOfLines.length - 1; i++) {
+			cities.add(new City(arrayOfLines[i]));
+		}
+		
+		
 		
 		//making the buttons
 		this.dButton = new DistanceButton("Shortest Distance", this, this.mPanel, this.dPanel, this.pPanel);
 		this.tButton = new TimeButton("Shortest Time",this, this.mPanel, this.dPanel, this.pPanel);
 	
 		//making the comboboxes
-		String[] capitals = {"Choose a City " ,"Olympia", "Indianapolis", "Baton Rouge", "Tucson"};
+		
+		ArrayList<City> city = cities;
+		
+		Object[] stuff = new Object[city.size()];
+		
+		for(int i = 0; i < city.size(); i++){
+			stuff[i] = city.get(i).CityName;
+		}
+		
 		//this.start = new JComboBox(capitals);
-		this.start = new StartBox(capitals, this.pPanel);
+		this.start = new StartBox(stuff, this.pPanel);
 		start.setSelectedIndex(0);
-		this.end = new EndBox(capitals, this.pPanel);
+		this.end = new EndBox(stuff, this.pPanel);
 		end.setSelectedIndex(0);
 		
 		//making the labels
--- EndBox.java
+++ EndBox.java
@@ -1,4 +1,6 @@
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 
@@ -19,10 +21,33 @@ public class EndBox<E> extends JComboBox<E> {
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		
+		
+		ReadFile file1 = new ReadFile(
+				"C:/EclipseWorkspaces/csse230/CrossCountryTouring/src/CapitalInfo");
+		String[] arrayOfLines = null;
+		try {
+			arrayOfLines = file1.OpenFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<City> cities = new ArrayList<City>();
+		for (int i = 0; i < arrayOfLines.length - 1; i++) {
+			cities.add(new City(arrayOfLines[i]));
+		}
+
+		int j = 0;
+		for(int i = 0; i<cities.size();i++){
+			if(  (cities.get(i).CityName.compareTo(this.getSelectedItem().toString()))==0 ){
+				j=i;
+			}
+		}
+		
 		this.pPanel.aboutDest.setText(null);
 		this.pPanel.aboutDest.insert("Destination City: "+this.getSelectedItem()
-				+"\nRating: "
-				+"\nPopulation: ",0);
+				+"\nRating: " + cities.get(j).Rating
+				+"\nPopulation: "+ cities.get(j).Population,0);
 	}
 	
 	

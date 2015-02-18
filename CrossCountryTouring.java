9e54bedd0f5503bf47ad78dc0c295ed203126eba
kfclee <katherinefc.lee@gmail.com>
2015-02-18 12:52:24 -0500

    CCT
    Make sure you put your correct file path!

--- CrossCountryTouring.java
+++ CrossCountryTouring.java
@@ -1,23 +1,17 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class CrossCountryTouring {
+
+	public static ArrayList<City> cities;
 	
-	public static void main(String[] args) throws IOException{
-	
-	CCFrame newFrame = new CCFrame();
-	newFrame.setVisible(true);
-	newFrame.setTitle("CrossCountryTouring");
-	
-	ReadFile file1 = new ReadFile("C:/EclipseWorkspaces/csse220/230 Project/src/CapitalInfo");
-	String[] arrayOfLines = file1.OpenFile();
-	ArrayList<City> cities = new ArrayList<City>();
-	for(int i = 0; i<arrayOfLines.length; i++){
-		cities.add(new City(arrayOfLines[i]));
+	public static void main(String[] args) throws IOException {
+
+		ReadFile file1 = new ReadFile(
+				"C:/EclipseWorkspaces/csse230/CrossCountryTouring/src/CapitalInfo");
+		String[] arrayOfLines = file1.OpenFile();
+		Graph USGraph = new Graph(arrayOfLines);
+
 	}
-	
-	
-	}
-	
+
 }
